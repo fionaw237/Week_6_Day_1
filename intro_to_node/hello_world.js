d664b1df4ac1af40def7627195e22469996a34c2
Fiona Wilson <fionaw237@gmail.com>
2018-08-20 10:13:28 +0100

    Initial commit

---
+++ intro_to_node/hello_world.js
@@ -0,0 +1,12 @@
+// console.log("Hello World!");
+
+// var name = "Mickey";
+
+// let name = "Mickey"
+const name = "Mickey"
+
+// typeof name will give 'string'
+// can only do string interpolation if the string is between backticks - ``
+
+name = "Minnie"
+console.log(name);
